243f200033b3377dac267912fda81fdae92787e3
mirolli <134691689+mirolli@users.noreply.github.com>
2025-07-19 16:57:41 +0800

    Update and rename m3u.js to thetvapp.js

--- pages/api/thetvapp.js
+++ pages/api/thetvapp.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default async function handler(req, res) {
   try {
     // 从 GitHub 拉取 m3u 文件内容
-    const response = await axios.get('https://raw.githubusercontent.com/mikekaprielian/rtnaodhor93n398/refs/heads/main/en/videoall.m3u');
+    const response = await axios.get('https://raw.githubusercontent.com/Drewski2423/DrewLive/refs/heads/main/TheTVApp.m3u8');
     
     // 设置响应头，返回 m3u 文件
     res.setHeader('Content-Type', 'application/x-mpegURL');
